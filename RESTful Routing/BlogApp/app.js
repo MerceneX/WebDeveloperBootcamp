bbb66280318deb340805a70d0b02ce780cfe7f58
MarkoTheStudent <marko.gluhak@student.um.si>
2019-02-10 22:18:36 +0100

    Almost done with REST

--- RESTful Routing/BlogApp/app.js
+++ RESTful Routing/BlogApp/app.js
@@ -1,12 +1,14 @@
-const express = require("express"),
-    app = express(),
+const methodOverride = require("method-override"),
     bParser = require("body-parser"),
-    mongoose = require("mongoose");
+    mongoose = require("mongoose"),
+    express = require("express"),
+    app = express();
 
 mongoose.connect("mongodb://localhost/restful_blog_app");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
 var blogSchema = new mongoose.Schema({
     title: String,
@@ -40,6 +42,60 @@ app.get("/", function(req, res){
 
 app.get("/blogs/new", function(req, res){
     res.render("new");
-})
+});
 
-app.listen(30
+app.post("/blogs", function(req, res){
+    Blog.create(req.body.blog, function(err, newBlog){
+        if(err){
+            res.render("new");
+        } else {
+            res.redirect("/blogs");
+        }
+    });
+});
+
+app.get("/blogs/:id", function(req, res){
+    Blog.findById(req.params.id, function(err, foundBlog){
+        if(err){
+            console.log(err);
+            res.redirect("/blogs");
+        } else {
+            res.render("show", {blog: foundBlog});
+        }
+    });
+});
+
+app.get("/blogs/:id/edit", function(req, res){
+    Blog.findById(req.params.id,function(err, foundBlog){
+        if(err){
+            console.log(err);
+        } else {
+            res.render("edit",{blog:foundBlog});
+        }
+    });
+});
+
+app.put("/blogs/:id",function(req,res){
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
+        if(err){
+            console.log(err);
+        } else {
+            res.redirect("/blogs/"+req.params.id);
+        }
+    });
+});
+
+app.delete("/blogs/:id", function(req, res){
+    Blog.findByIdAndRemove(req.params.id, function(err){
+        if(err){
+            console.log(err);
+        }else{
+            res.redirect("/blogs");
+        }
+    });
+});
+
+
+app.listen(3000, "localhost", function(){
+    console.log("Server started");
+});
